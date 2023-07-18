e1257c96892d48b212e1428f35e86a66ddeeb4dd
Mitko Milev <milevk2@gmail.com>
2023-07-19 02:11:42 +0300

    Added a view with tags for user's convenience

--- Views/tagView.js
+++ Views/tagView.js
@@ -1,34 +1,79 @@
 import { render, html } from "../node_modules/lit-html/lit-html.js"
+import { submitHandler } from "../submitLogic.js";
 import { removeProjectInfo } from "../utility.js";
 
-function showTagView() {
+function tagsView(){
 
-    removeProjectInfo();
-    render(tagTemplate(), document.querySelector('#main'));
+removeProjectInfo();
+render(tagsViewTemplate, document.querySelector('#main'));
 }
 
-const tagTemplate = () => html`<div class=" container mt-5">
+export{tagsView}
 
-    <div class="row mb-3 mt-3">
-        <div class="col-md-6">
-            
-            <a href="/Auxilia"><span class="badge rounded-pill text-bg-secondary">Auxilia</span></a>
-            <a href="/Legiones"><span class="badge rounded-pill text-bg-secondary">Legiones</span></a>
-            <a href="/Cohortes Praetoria"><span class="badge rounded-pill text-bg-secondary">Cohortes Praetoria</span></a>
-            <a href="/Classis"><span class="badge rounded-pill text-bg-secondary">Classis</span></a>
-            <a href="/Надгробни паметници"> <span class="badge rounded-pill text-bg-secondary">Надгробен паметник</span></a>
-            <a href="/Посветителни надписи"> <span class="badge rounded-pill text-bg-secondary">Посветителен надпис</span></a>
-            
+const tagsViewTemplate =  html`
+
+<div class="container mt-3">
+
+<form @submit="${submitHandler}">
+        <div class="row">
+            <div class="col">
+                <label>Име</label>
+                <input class="form-control me-2"></input>
+            </div>
+            <div class="col">
+                <label>Име (бг)</label>
+                <input class="form-control me-2"></input>
+            </div>
+            <div class="col">
+                <label>Период (от):</label>
+                <input class="form-control me-2"></input>
+            </div>
+            <div class="col">
+                <label>Период (до):</label>
+                <input class="form-control me-2"></input>
+            </div>
         </div>
-    </div>
-
-    <div class="row mb-3 mt-3">
-        <div class="col-md-6">
-        <a href="/Военни дипломи"><span class="badge rounded-pill text-bg-secondary">Военна диплома</span></a>
+        <div class="row">
+            <div class="col m-2">
+                <button type="submit" class="btn btn-primary">Търсене</button>
+            </div>
         </div>
-    </div>
+    </form>
 
-</div>
+ <h3 class="underlined">
+    Списък с императори
+ </h3>
+
+ <table class="table">
+  <thead class>
+    <tr>
+      <th scope="col">Име</th>
+      <th scope="col">Име (Бг)</th>
+      <th scope="col">Период</th>
+      <th scope="col">Свързани документи</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row"><a href="/Emperors/Germanicus Iulius Caesar">Germanicus Iulius Caesar</a></th>
+      <td>Германик Юлий Цезар</td>
+      <td>AD 14 - 19</td>
+      <td>	
+      Germanici Caesaris</td>
+    </tr>
+    <tr>
+      <th scope="row"><a href="/Emperors/Imperator Caesar Augustus">Imperator Caesar Augustus</a></th>
+      <td>Император Цезар Август</td>
+      <td>BC 27 - 14</td>
+      <td>divi Augusti</td>
+    </tr>
+    <tr>
+     <th scope="row"><a href="/Emperors/Lucius Septimius Severus Pertinax">Lucius Septimius Severus Pertinax</a></th>
+     <td>Луций Септимий Север</td>
+     <td>AD 193-211</td>
+     <td>Augggustorum nnnostrorum</td>
+    </tr>
+  </tbody>
+ </table>
+ </div>
 `
-
-export { showTagView }
